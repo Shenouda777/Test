7350a607eea8e7efd67b14034df6b6736a00b0e8
unknown <nursaddel@yahoo.com>
2023-01-28 11:49:49 -0500

    first commit

---
+++ Folder/Dojanary2.js
@@ -0,0 +1,10 @@
+var popup = document.querySelector ('popup') 
+setTimeout (alert, 1000)
+
+function discount () { 
+    popup.style. display = "block"
+
+}
+function popup ( ) {
+    popup.remove ()
+}
